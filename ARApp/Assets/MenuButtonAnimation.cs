d720d980c7d624549dbf721b3cc262970f979c99
Wikipedia17 <annamstoneman@gmail.com>
2021-03-15 16:40:50 -0700

    Added project

---
+++ ARApp/Assets/MenuButtonAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonAnimation : MonoBehaviour
+{
+ 	public GameObject menuButtons;
+	public void showHideMenu()
+	{
+		if(menuButtons != null)
+		{
+			Animator animator = menuButtons.GetComponent<Animator>();
+			if(animator != null)
+			{
+				bool isOpen = animator.GetBool("show");
+				animator.SetBool("show", !isOpen);
+			}
+		}
+	}
+	
+}
